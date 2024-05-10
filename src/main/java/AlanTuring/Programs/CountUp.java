65c49393dfa28f813b64f618984cdc468d694fbe
Patrik <psirb@myuax.com>
2024-05-10 15:34:48 +0200

    Clase CountUp con su respectivo metodo CountUp que hace una cuenta progresiva hasta llegar a 5 que es el valor que le hemos asignado a la variable i.

---
+++ src/main/java/AlanTuring/Programs/CountUp.java
@@ -0,0 +1,11 @@
+package AlanTuring.Programs;
+
+public class CountUp {
+    public void countUp(){
+        int i = 0;
+        while(i <= 5){   //Vamos a hacer un loop que imprime los valores de i desde 0 hasta 5, es decir, hace una cuenta progresiva.
+            System.out.println(i);
+            i++;
+        }
+    }
+}
