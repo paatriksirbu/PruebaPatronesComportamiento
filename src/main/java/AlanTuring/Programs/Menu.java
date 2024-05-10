e00d9f7a4c7da7e83a840b2b3c46db8b9e104898
Patrik <psirb@myuax.com>
2024-05-10 15:58:06 +0200

    Menu de los programas establecidos por el momento. CountDown y CountUp

---
+++ src/main/java/AlanTuring/Programs/Menu.java
@@ -0,0 +1,35 @@
+package AlanTuring.Programs;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void mostrarMenu(){
+        System.out.println("    - Menu-");
+        System.out.println("1. Cuenta regresiva");
+        System.out.println("2. Cuenta progresiva");
+        System.out.println("3. Salir");
+
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        while(opcion != 3){
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+            switch(opcion){
+                case 1:
+                    CountDown cd = new CountDown();
+                    cd.countDown();
+                    break;
+                case 2:
+                    CountUp cu = new CountUp();
+                    cu.countUp();
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }
+    }
+}
