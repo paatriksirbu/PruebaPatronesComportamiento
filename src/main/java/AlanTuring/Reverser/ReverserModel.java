7a27d6a62fdd5e13997684afc8a7832e74567818
Patrik <psirb@myuax.com>
2024-05-12 10:14:55 +0200

    Clase Reverser Model

--- src/main/java/AlanTuring/Reverser/ReverserModel.java
+++ src/main/java/AlanTuring/Reverser/ReverserModel.java
@@ -1,4 +1,15 @@
 package AlanTuring.Reverser;
 
 public class ReverserModel {
+    public String simulateCountUp(){    //Loop infinito
+        return "Counting up...";
+    }
+
+    public String simulateCountDown(){  //Simula un loop regresivo que terminara al llegar a 0.
+        return "Counting down...";
+    }
+
+    public String simulateReverser(){ //Simula la funcion del reverser(Hacer lo contrario de lo que se le pide)
+        return "Reversing...";
+    }
 }
