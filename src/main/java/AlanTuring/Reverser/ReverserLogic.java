5cd3cfd5de0ebce577f4e9861b53072b3576ff6d
Patrik <psirb@myuax.com>
2024-05-12 10:36:41 +0200

    Clase Reverser Logic y completamos el modelo vista controlador.

--- src/main/java/AlanTuring/Reverser/ReverserLogic.java
+++ src/main/java/AlanTuring/Reverser/ReverserLogic.java
@@ -1,4 +1,21 @@
 package AlanTuring.Reverser;
 
+import java.util.Random;
+
 public class ReverserLogic {
+
+    public static boolean willHalt(){
+        return true; // Simulamos que siempre va a terminar y no se va a quedar en un bucle infinito.
+    }
+
+    public static boolean simulateHaltingProblem(){
+        Random rndm = new Random();
+        int rndmNum = rndm.nextInt();
+        return rndmNum % 2 == 0;
+    }
+
+    public static boolean simulateNonHaltingProblem(){
+        return false;
+    }
+
 }
