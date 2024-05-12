6152968711bfa8c28d45ad273c28fd4ceeaf50e8
Patrik <psirb@myuax.com>
2024-05-12 19:11:49 +0200

    Analizador de parentesis en el codigo.

--- src/main/java/CodeAnalyzer/Analyzer/Analyzers/ParenthesisHandler.java
+++ src/main/java/CodeAnalyzer/Analyzer/Analyzers/ParenthesisHandler.java
@@ -1,4 +1,14 @@
 package CodeAnalyzer.Analyzer.Analyzers;
 
-public class ParenthesisHandler {
+import CodeAnalyzer.Handler.HandlerClass;
+
+public class ParenthesisHandler extends HandlerClass {
+
+    @Override
+    public String handleRequest(String request) {
+        if (request.contains("(") || request.contains(")")){
+            return "Parenthesis found in code." + super.handleRequest(request);
+        }
+        throw new RuntimeException("Missing parenthesis in code.");
+    }
 }
